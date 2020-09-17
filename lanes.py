6645468716f0a03e4816e3894b51fb6fe6913862
Andrew Massart <amassart@asu.edu>
2020-09-17 00:25:15 -0400

    Apply Gaussian Blur to grayscale image

--- lanes.py
+++ lanes.py
@@ -3,7 +3,10 @@ import numpy as np
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
+
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 
-cv2.imshow('result', gray)
+gauss_blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+cv2.imshow('result', gauss_blur)
 cv2.waitKey(0)
