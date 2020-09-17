ea69affb6efcd09ab2453bdf66a21a9d3bf03518
Andrew Massart <amassart@asu.edu>
2020-09-17 00:53:21 -0400

    Create mask for field of view

--- lanes.py
+++ lanes.py
@@ -1,14 +1,25 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    gauss_blur = cv2.GaussianBlur(gray, (5,5), 0)
+    canny = cv2.Canny(gauss_blur, 50, 150)
+    return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+    [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+canny = canny(lane_image)
 
-gauss_blur = cv2.GaussianBlur(gray, (5,5), 0)
-
-canny = cv2.Canny(gauss_blur, 50, 125)
-
-cv2.imshow('result', canny)
+cv2.imshow('result', region_of_interest(canny))
 cv2.waitKey(0)
