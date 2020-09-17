d1c69193be1d1e7181507b0657c10759e0d82893
Andrew Massart <amassart@asu.edu>
2020-09-17 01:58:19 -0400

    Optimized detected lines on overlay

--- lanes.py
+++ lanes.py
@@ -2,6 +2,33 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5)) #only draw lines to 3/5 screen height
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+
+    return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line= make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     gauss_blur = cv2.GaussianBlur(gray, (5,5), 0)
@@ -11,8 +38,7 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 255), 10) # BGR nor RGB
     return line_image
 
@@ -28,10 +54,11 @@ def region_of_interest(image):
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = canny(lane_image)
-image_with_mask_applied = region_of_interest(canny)
+canny_image = canny(lane_image)
+image_with_mask_applied = region_of_interest(canny_image)
 hough_lines = cv2.HoughLinesP(image_with_mask_applied, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-line_image = display_lines(lane_image, hough_lines)
+optimized_lines = average_slope_intercept(lane_image, hough_lines)
+line_image = display_lines(lane_image, optimized_lines)
 lane_image_with_lines = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
 cv2.imshow('result', lane_image_with_lines)
