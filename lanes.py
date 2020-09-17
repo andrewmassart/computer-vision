e173aaca01d557d228aa7c0bca44ccb831b077fc
Andrew Massart <amassart@asu.edu>
2020-09-17 01:24:52 -0400

    Use Hough transform to show detected lines

--- lanes.py
+++ lanes.py
@@ -1,11 +1,20 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     gauss_blur = cv2.GaussianBlur(gray, (5,5), 0)
     canny = cv2.Canny(gauss_blur, 50, 150)
     return canny
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (0, 75, 255), 10) # BGR nor RGB
+    return line_image
 
 def region_of_interest(image):
     height = image.shape[0]
@@ -21,6 +30,8 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 image_with_mask_applied = region_of_interest(canny)
+hough_lines = cv2.HoughLinesP(image_with_mask_applied, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, hough_lines)
 
-cv2.imshow('result', image_with_mask_applied)
+cv2.imshow('result', line_image)
 cv2.waitKey(0)
